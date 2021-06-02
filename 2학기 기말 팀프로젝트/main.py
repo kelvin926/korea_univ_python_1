92ea657856acb0415b260a3756865d0dbe2e59ce
kelvin926 <kelvin926@naver.com>
2021-06-03 05:55:29 +0900

    기말고사 팀플 중간점검 5시55분

--- 2학기 기말 팀프로젝트/main.py
+++ 2학기 기말 팀프로젝트/main.py
@@ -13,53 +13,122 @@
 
 import csv
 
-rain = open('py1_univ/2학기 기말 팀프로젝트/csv_file/rain.csv', 'r', encoding='cp949')
-dust = open('py1_univ/2학기 기말 팀프로젝트/csv_file/dust.csv', 'r', encoding='cp949')
-tem = open('py1_univ/2학기 기말 팀프로젝트/csv_file/tem.csv', 'r', encoding='cp949')
-rain_data = csv.reader(rain)
-header_1 = next(rain_data)
+while(1):
+    
+    now = input("오늘의 날짜를 입력해주세요. ex. \'2021-01-01\' : ")
 
-dust_data = csv.reader(dust)
-header_2 = next(dust_data)
-
-tem_data = csv.reader(tem)
-header_3 = next(tem_data)
-
-q_rain = input("날짜를 입력해주세요. ex. \'2021-01-01\'")
-
-print("강수량 갑니당") #debug
-rain_index = []
-rain_now_index = []
-for row_rain in rain_data: # 강수량 디버깅
-    # print(row_rain)
-    rain_index.append(row_rain)
-    # print(rain_index)
-
-for i in range(len(rain_index)):
-    if q_rain == rain_index[i][0]:
-        print("무야호")
-        print(rain_index[i][0:]) # 인덱스 확인용 디버깅
-        rain_now_index = rain_index[i][0:] # 인덱스 적용
-
-if rain_now_index[2] == '': #비가 내리지 않았을 때
-    print("이 날에는 비가 내리지 않았습니다")
-else:
-    print("이 날에는 {}mm 의 비가 내렸습니다.".format(rain_now_index[2]))
-
-print("강수량 완료")
-
-rain.close()
+    # 작년의 오늘을 구합니다.
+    now_year = int(now[0:4])
+    past_day = str(now_year-1)+now[4:]
+    print("1년전은 {} 입니다.".format(past_day))
 
 
-
-# print("미세먼지 갑니당") #debug
-# for row_dust in dust_data: # 미세먼지 디버깅
-#     print(row_dust)
-# dust.close()
+    select = int(input("무엇을 실행할까요?\n1.강수량\n2.미세먼지\n3.기온\n숫자로 입력해주세요! : "))
 
 
+    if select == 1: # 강수량
+        rain = open('py1_univ/2학기 기말 팀프로젝트/csv_file/rain.csv', 'r', encoding='cp949')
+        rain_data = csv.reader(rain)
+        header_1 = next(rain_data)
+        print("강수량 갑니당") #debug
+        rain_index = []
+        rain_now_index = []
+        rain_past_index = []
+        for row_rain in rain_data: # 강수량 디버깅
+            # print(row_rain)
+            rain_index.append(row_rain) # 2차 배열 만듦.
+            # print(rain_index)
 
-# print("기온 갑니당") #debug
-# for row_tem in tem_data: # 기온 디버깅
-#     print(row_tem)
-# tem.close()
+
+        ''' #현재 날씨를 확인하는 부분임. 따라서 비활성화
+        for i in range(len(rain_index)):
+            if q_rain == rain_index[i][0]:
+                print("해당 날짜 찾기 완료")
+                print(rain_index[i][0:]) # 인덱스 확인용 디버깅
+                rain_now_index = rain_index[i][0:] # 인덱스 적용
+        '''
+
+        # 작년, 오늘의 날씨를 구하는 부분
+        for i in range(len(rain_index)):
+            if str(past_day) == str(rain_index[i][0]):
+                print("작년의 오늘을 찾았습니다.")
+                print(rain_index[i][0:]) # 인덱스 확인용 디버깅
+                rain_past_index = rain_index[i][0:] # 인덱스 적용
+                print(rain_past_index) # rain_past_index = 작년의 데이터
+
+        # 구동부
+
+        if rain_index[i][2] == '': #비가 내리지 않았을 때, rain_past_index 리스트를 쓰면 사이즈 오버 오류가 발생하므로 의도적으로 원본 리스트에서 가져옴.
+            print("이 날에는 비가 내리지 않았습니다")
+        else:
+            print("이 날에는 {}mm 의 비가 내렸었습니다.".format(rain_past_index[2]))
+
+        print("강수량 완료")
+
+        rain.close()
+
+
+    if select == 2: # 미세먼지
+        dust = open('py1_univ/2학기 기말 팀프로젝트/csv_file/dust.csv', 'r', encoding='cp949')
+        dust_data = csv.reader(dust)
+        header_2 = next(dust_data)
+
+        print("미세먼지 갑니당") #debug
+        dust_index = []
+        dust_now_index = []
+        dust_past_index = []
+        for row_dust in dust_data: # 미세먼지 디버깅
+            dust_index.append(row_dust) # 2차 배열 만듦.
+
+        # 작년, 오늘의 미세먼지를 구하는 부분
+        for i in range(len(dust_index)):
+            if str(past_day) == str(dust_index[i][2]):
+                print("작년의 오늘을 찾았습니다.")
+                print(dust_index[i][0:]) # 인덱스 확인용 디버깅
+                dust_past_index = dust_index[i][0:] # 인덱스 적용
+                print(dust_past_index) # dust_past_index = 작년의 데이터
+
+        # 구동부
+
+        if dust_past_index[3] == '': # 미세먼지가 존재하지 않았을 때
+            print("이 날의 과거 미세먼지 데이터가 존재하지 않습니다.")
+        else:
+            print("이 날에는 {}의 미세먼지가 있었습니다.".format(dust_past_index[3]))
+
+        print("미세먼지 완료")
+
+
+        dust.close()
+
+
+
+    if select == 3: # 기온
+        tem = open('py1_univ/2학기 기말 팀프로젝트/csv_file/tem.csv', 'r', encoding='cp949')
+        tem_data = csv.reader(tem) 
+        header_3 = next(tem_data) #평균,저,고
+
+        print("기온 갑니당") #debug
+        tem_index = []
+        tem_now_index = []
+        tem_past_index = []
+        for row_tem in tem_data: # 기온 디버깅
+            tem_index.append(row_tem) # 2차 배열 만듦.
+
+        # 작년, 오늘의 기온(평균,최저,최고)을 구하는 부분
+        for i in range(len(tem_index)):
+            if str(past_day) == str(tem_index[i][2]):
+                print("작년의 오늘을 찾았습니다.")
+                print(tem_index[i][0:]) # 인덱스 확인용 디버깅
+                tem_past_index = tem_index[i][0:] # 인덱스 적용
+                print(tem_past_index) # tem_past_index = 작년의 데이터
+
+        # 구동부
+
+        if tem_past_index[3] == '': # 기온가 존재하지 않았을 때
+            print("이 날의 과거 미세먼지 데이터가 존재하지 않습니다.")
+        else:
+            print("이 날에는 {}의 미세먼지가 있었습니다.".format(dust_past_index[3]))
+
+        print("기온 완료")
+
+        tem.close()
